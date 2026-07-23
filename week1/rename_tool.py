0e91f3550758e7d168760ee57d39127e16080eaa
Leo-learner <leo12345.gmail@gmail.com>
2026-07-23 11:10:38 +0800

    用with_stem替代了原有的with_name，确保不改文件后缀

--- week1/rename_tool.py
+++ week1/rename_tool.py
@@ -1,14 +1,13 @@
 from pathlib import Path
+dry_run = True
 p = Path('week1/rename_test')
-# i = 0
-# for item in p.iterdir():
-#     i += 1
-#     item.stem = '照片_' + f"{i:02}" 
-#     item.suffix = '.jpg'
-#     print(item.name)
 items = list(p.iterdir())
-for i, item in enumerate(items, start=1):
-    new_name = f"照片_{i:02}.jpg"
-    new_path = item.with_name(new_name)
-    item.rename(new_path)
-    print(f"Renamed {item.name} to {new_name}")
+sorted_items = sorted(items, key=lambda x: x, reverse=False)
+for i, item in enumerate(sorted_items, start=1):
+    new_stem = f"照片_{i:02}"
+    new_path = item.with_stem(new_stem)
+    if not dry_run:
+        item.rename(new_path)
+        print(f"Renamed {item.name} to {new_path.name}")
+    else:
+        print(f"Dry run: {item.name} would be renamed to {new_path.name}")
