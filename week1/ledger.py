0c658a2b2f24db27f838d35e377d648fb1cf6404
Leo-learner <leo12345.gmail@gmail.com>
2026-07-25 14:31:43 +0800

    修复了第32行的重复sorted错误，去掉了多余的sorted，修改了定义的函数名，将INPUT()和INPUT_NUM()改为了input_str()和input_num()，并将参数名从str改为prompt，使其符合python函数命名和参数命名规范，将is_blank()函数精简为了return input_str == ，去掉了第15行的后半段or is_blank死逻辑，将if records  == []改为了if not records，补充了choice == '0'和其他输入的情况，确保程序能够正常退出并在用户输入错误选项时不崩溃

--- week1/ledger.py
+++ week1/ledger.py
@@ -4,14 +4,14 @@ def is_blank(input_str):
     else:
         return False
 
-def INPUT(str):
-    input_str = input("请输入" + str + ": ").strip()
+def input_str(prompt):
+    input_str = input("请输入" + prompt + ": ").strip()
     while is_blank(input_str):
         input_str = input("输入不能为空，不能包含空格或其他符号，请重新输入: ").strip()
     return input_str
 
-def INPUT_NUM(str):
-    input_str = input("请输入" + str + ": ").strip()
+def input_num(prompt):
+    input_str = input("请输入" + prompt + ": ").strip()
     while not input_str.isdigit() or is_blank(input_str):
         input_str = input("输入必须为整数且不能为空，不能包含空格或其他字符，请重新输入: ").strip()
     return int(input_str)
@@ -22,27 +22,39 @@ while True:
     print("2. 按金额排序列出全部")
     print("3. 分类汇总")
     print("0. 退出")
-    choice = input("请选择操作: ")
+    choice = input("请选择操作")
     if choice == "1":
-        category = INPUT("分类")
-        amount = INPUT_NUM("金额")
-        notes = INPUT("备注")
+        category = input_str("分类")
+        amount = input_num("金额")
+        notes = input_str("备注")
         records.append({"分类": category, "金额": amount, "备注": notes})
     elif choice == "2":
-        print(sorted(sorted(records, key=lambda r: r["金额"])))
+        if not records:
+            print("\n没有支出记录\n")
+            continue
+        for record in sorted(records, key=lambda r: r["金额"]):
+            print(f"分类: {record['分类']}, 金额: {record['金额']:.2f}, 备注: {record['备注']}")
     elif choice == "3":
-        if records == []:
+        if not records:
             print("\n没有支出记录\n")
             continue
         summary = {}
+        number = {}
         for record in records:
             category = record["分类"]
-            amount = float(record["金额"])
+            amount = record["金额"]
             if category in summary:
                 summary[category] += amount
+                number[category] += 1
             else:
                 summary[category] = amount
+                number[category] = 1
         for category, total in summary.items():
-            print(f"{category}: {total:.2f }")
+            print(f"{category}: {total:.2f}, 笔数: {number[category]}")
+    elif choice == "0":
+        print("退出系统，感谢使用!")
+        break
+    else:
+        print("无效的选择，请重新输入。")
 
 #支出记录之所以用list而不用dict是因为支出记录可能包含相同的分类和金额，而字典要求键是唯一的。如果使用字典来存储支出记录，可能会导致数据覆盖或丢失。
