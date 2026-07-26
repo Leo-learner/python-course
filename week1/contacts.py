89141a5f6f954238a2fc7e575b9f02138440ebb8
Leo-learner <leo12345.gmail@gmail.com>
2026-07-26 15:05:40 +0800

    用add_contact(contacts, name, phone)、fond_contact(contacts, name)、show_all(contacts)这几个函数替代了之前main函数中的具体代码，精简了main函数的结构

--- week1/contacts.py
+++ week1/contacts.py
@@ -10,6 +10,18 @@ def num_blank_resolver(input_str):
     while is_blank(input_str) or not input_str.isdigit():
         input_str = input("输入不能为空，且必须为数字，请重新输入: ").strip()
     return int(input_str)
+def show_all_contacts():
+    if contacts:
+        print("所有联系人:")
+        for name, phone in contacts.items():
+            print(f"{name} - {phone}")
+    else:
+        print("通讯录为空")
+def add_contact(contacts, name, phone):
+    contacts[name] = phone
+    print(f"已添加联系人: {name} - {phone}")
+def find_contact(contacts, name):
+    return contacts.get(name, None)
 
 while True:
     print("\n1. 添加联系人")
@@ -31,8 +43,7 @@ while True:
                 print("取消更新。")
         else:
             phone = num_blank_resolver(input("请输入联系人电话: "))
-            contacts[name] = phone
-            print(f"已添加联系人: {name} - {phone}")
+            add_contact(contacts, name, phone)
     elif choice == '2':
         name = blank_resolver(input("请输入要删除的联系人姓名: "))
         if name in contacts:
@@ -42,17 +53,13 @@ while True:
             print(f"联系人 {name} 不存在")
     elif choice == '3':
         name = blank_resolver(input("请输入要查找的联系人姓名: "))
-        if name in contacts:
-            print(f"联系人信息: {name} - {contacts[name]}")
+        contact = find_contact(contacts, name)
+        if contact:
+            print(f"联系人信息: {name} - {contact}")
         else:
             print(f"联系人 {name} 不存在")
     elif choice == '4':
-        if contacts:
-            print("所有联系人:")
-            for name, phone in contacts.items():
-                print(f"{name} - {phone}")
-        else:
-            print("通讯录为空")
+        show_all_contacts()
     elif choice == '0':
         print("退出系统，感谢使用!")
         break
