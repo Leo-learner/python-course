b488c8fba2eda04fc6b7686362d44c34bff8c2b2
Leo-learner <leo12345.gmail@gmail.com>
2026-07-26 15:40:19 +0800

    把第10行的Error改为了Waring

--- week1/rename_tool.py
+++ week1/rename_tool.py
@@ -1,14 +1,13 @@
 from pathlib import Path
 dry_run = True
 p = Path('week1/rename_test')
-# items = list(p.iterdir())
 items = [item for item in p.iterdir() if item.is_file()]
 sorted_items = sorted(items)
 for i, item in enumerate(sorted_items, start=1):
     new_stem = f"照片_{i:02}"
     new_path = item.with_stem(new_stem)
     if new_path.exists():
-        print(f"Error: {new_path.name} already exists. Skipping {item.name}.")
+        print(f"Warning: {new_path.name} already exists. Skipping {item.name}.")
         continue
     if not dry_run:
         item.rename(new_path)
