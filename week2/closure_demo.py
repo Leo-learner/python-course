a8ae62340c52462da707500512a525c27d33d2c7
Leo-learner <leo12345.gmail@gmail.com>
2026-07-30 16:32:23 +0800

    用闭包函数做了一个计数器

---
+++ week2/closure_demo.py
@@ -0,0 +1,14 @@
+def make_counter():
+    count = 0
+    def counter():
+        nonlocal count
+        count = count + 1
+        return count
+    return counter
+
+c1 = make_counter()
+c2 = make_counter()
+print(c1())
+print(c1())
+print(c1())
+print(c2())
