caa42436f925baf014fe08b183ccac43c5951c60
Leo-learner <leo12345.gmail@gmail.com>
2026-08-01 15:22:19 +0800

    用闭包函数写了一个带参数的装饰器

---
+++ week2/repeat.py
@@ -0,0 +1,17 @@
+import time
+import functools
+def repeat(times):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+@repeat(times=0)
+def greet():
+    print("hi")
+
+greet()
