9b47cf6db702475af56aa73be60a281a2b596bf6
Leo-learner <leo12345.gmail@gmail.com>
2026-08-02 14:46:33 +0800

    做了class的REPL

--- week1/REPL_sorted.py
+++ week1/REPL_sorted.py
@@ -1,10 +1,33 @@
-records = [
-    {"分类": "餐饮", "金额": "33.5"},
-    {"分类": "交通", "金额": "12.0"},
-    {"分类": "餐饮", "金额": "88.0"},
-]
-print(sorted(records, key=lambda r: r["金额"]))
+# records = [
+#     {"分类": "餐饮", "金额": "33.5"},
+#     {"分类": "交通", "金额": "12.0"},
+#     {"分类": "餐饮", "金额": "88.0"},
+# ]
+# print(sorted(records, key=lambda r: r["金额"]))
 
-print(sorted(records, key=lambda r: r["金额"], reverse=True))
+# print(sorted(records, key=lambda r: r["金额"], reverse=True))
 
-print(sorted(records, key=lambda r: (r["分类"], r["金额"])))
+# print(sorted(records, key=lambda r: (r["分类"], r["金额"])))
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} 说：汪")
+
+    def birthday(self):
+        self.age += 1
+        return self.age
+
+d1 = Dog("旺财", 3)
+d2 = Dog("小黑", 5)
+
+d1.name
+d2.name
+d1.bark()
+d2.bark()
+d1.birthday()
+d1.age
+d2.age
