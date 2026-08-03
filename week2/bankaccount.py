470ce21259fe94f6a7e2aeeff09543456864b042
Leo-learner <leo12345.gmail@gmail.com>
2026-08-03 16:01:44 +0800

    加上了主动抛异常的功能，存取款金额小于等于零或者取款金额大于存款余额会主动报错并提醒用户

--- week2/bankaccount.py
+++ week2/bankaccount.py
@@ -1,16 +1,28 @@
+class BankError(Exception):
+    pass
+class InvalidAmountError(BankError):
+    pass
+class InsufficientFundsError(BankError):
+    pass
 class BankAccount:
     def __init__(self, account, balance):
         self.account = account
         self.balance = balance
 
     def check(self):
-        print(f"{self.account}账户的余额为{self.balance}")
+        print(f"{self.account}账户的余额为：{self.balance}")
 
     def deposit(self, num):
+        if num <= 0:
+            raise ValueError(f"存款金额必须大于零！试图存款{num}")
         self.balance += num
         return self.balance
 
     def withdraw(self, num):
+        if num <= 0:
+            raise InvalidAmountError(f"取款金额必须大于零！试图取款{num}")
+        if num > self.balance:
+            raise InsufficientFundsError(f"余额不足：余额 {self.balance} 试图取款{num}")
         self.balance -= num
         return self.balance
 
@@ -23,4 +35,29 @@ print(a2.account)
 a1.check()
 a2.check()
 a1.deposit(100)
+a1.check()
+try:
+    a1.withdraw(15)
+except (ValueError, InvalidAmountError, InsufficientFundsError) as e:
+    print(e)
+a1.check()
+try:
+    a1.withdraw(5000)
+except (ValueError, InvalidAmountError, InsufficientFundsError) as e:
+    print(e)
+a1.check()
+try:
+    a1.deposit(-10)
+except (ValueError, InvalidAmountError, InsufficientFundsError) as e:
+    print(e)
+a1.check()
+try:
+    a1.withdraw(-10)
+except (ValueError, InvalidAmountError, InsufficientFundsError) as e:
+    print(e)
+a1.check()
+try:
+    a1.withdraw(0)
+except (ValueError, InvalidAmountError, InsufficientFundsError) as e:
+    print(e)
 a1.check()
